b0dac68efd8f6aaa3b886aab4e014a542a02d9ad
monta <djmonta@gmail.com>
2020-04-18 00:27:41 +0900

    :bell: Initial Commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,27 @@
+<header class="banner sans-serif">
+  <div class="container">
+    <!-- <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a> -->
+    <!-- <nav class="nav-primary">
+      @if (has_nav_menu('primary_navigation'))
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+      @endif
+    </nav> -->
+  </div>
+  <div class="cover vh-100 tc bg-light-red">
+    <div class="bg-red h-50"></div>
+    <nav class="dt w-100 mw8 center v-mid">
+      <div class="dtc w-50 v-mid pa3">
+        <a href="{{ home_url('/') }}" class="dib w2 h2 pa1 ba b--white-90 grow-large border-box">
+          <svg class="link white-90 hover-white" data-icon="skull" viewBox="0 0 32 32" style="fill:currentcolor"><title>skull icon</title><path d="M16 0 C6 0 2 4 2 14 L2 22 L6 24 L6 30 L26 30 L26 24 L30 22 L30 14 C30 4 26 0 16 0 M9 12 A4.5 4.5 0 0 1 9 21 A4.5 4.5 0 0 1 9 12 M23 12 A4.5 4.5 0 0 1 23 21 A4.5 4.5 0 0 1 23 12"></path></svg>
+        </a>
+      </div>
+      <div class="dtc v-mid tr pa3">
+        <a class="menu-hover no-underline white-70 dn db mv3 ph3" href="/" >How it Works</a>
+        <a class="menu-hover no-underline white-70 dn db mv3 ph3" href="/" >Pricing</a>
+        <a class="menu-hover no-underline white-70 dn db mv3 ph3" href="/" >About</a>
+        <a class="menu-hover no-underline white-70 dn db mv3 ph3" href="/" >Careers</a>
+        <!-- <a class="f6 fw4 hover-white menu-hover no-underline white-70 dib ml2 pv2 ph3 ba" href="/" >Sign Up</a> -->
+      </div>
+    </nav>
+  </div>
+</header>
