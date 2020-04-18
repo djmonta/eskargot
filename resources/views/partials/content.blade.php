9d1192ad0e238c2198040b29fa568a82e81fa040
monta <djmonta@gmail.com>
2020-04-18 18:10:33 +0900

    :up: update template (temporary)

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,7 +1,7 @@
 <article @php post_class() @endphp>
   <header>
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
     @include('partials/entry-meta')
+    <h2 class="entry-title sans-serif"><a href="{{ get_permalink() }}" class="no-underline">{!! get_the_title() !!}</a></h2>
   </header>
   <!-- <div class="entry-summary">
     @php the_excerpt() @endphp
--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,3 +1,3 @@
-<div class="page-header">
+<div class="page-header mid-gray arial-black tracked-mega pt5 tc">
   <h1>{!! App::title() !!}</h1>
 </div>
