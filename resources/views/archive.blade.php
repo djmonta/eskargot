c4189934deddc66b183529203101524d4149ee87
monta <djmonta@gmail.com>
2020-05-12 19:02:52 +0900

    アーカイブページにアイキャッチを表示する

---
+++ resources/views/archive.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+  @include('partials.page-header')
+
+  @if (!have_posts())
+    <div class="alert alert-warning">
+      {{ __('Sorry, no results were found.', 'sage') }}
+    </div>
+    {!! get_search_form(false) !!}
+  @endif
+
+  @while (have_posts()) @php the_post() @endphp
+    @include('partials.content-'.APP::getPostCategorySlug(get_the_ID()))
+  @endwhile
+
+  {!! get_the_posts_navigation() !!}
+
+  </div>
+@endsection
